9721a9ebb6effc1aa426ec0d912bfb3063d6d42c
judelin seide <seide_judelin@yahoo.fr>
2021-08-06 20:05:21 -0400

    sudoku and 2048 modifié

--- mon-app/src/index.js
+++ mon-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './appJeux/appSudoku';
+import App from './appJeux/AppRoute';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
